6ad1068aafd8c7cc4c17036844a30dbb902c5a57
mycaptainaashi <97294331+mycaptainaashi@users.noreply.github.com>
2022-01-07 20:03:14 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+list1 = [12, -7, 5, 64, -14] 
+
+for x in list1:
+    print(x)
+
+
+# In[18]:
+
+
+list1 = [12, -7, 5, 64, -14] 
+
+for x in list1:
+    if x >= 0 :
+        print (x , end = " , ")
+
+
+# In[19]:
+
+
+list2 = [12, 14, -95, 3] 
+
+for y in list2 :
+    if y >= 0 :
+        print (y , end = " , ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
